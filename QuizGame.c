88e887d228a130bda53f4933ce67cbebd852bc2a
Francisco Amado [1211763] <1211763@isep.ipp.pt>
2023-01-09 18:12:21 +0000

    Added new questions

--- QuizGame.c
+++ QuizGame.c
@@ -49,6 +49,8 @@ int main() {
             }
         }
 
+        printf("A resposta certa era: %s\n", getQuestionList().questionList[0].solution);
+
         printf("Segunda pergunta:\n");
         printf("%s\n", getQuestionList().questionList[1].problem);
         printf("%c. %s\n", KEY[0], getQuestionList().questionList[1].wrongAnswer1);
@@ -65,7 +67,131 @@ int main() {
 
         printf("\n");
 
-         for(int i = 1; i < numberOfPlayers + 1; i++) {
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            if (answer[i] == KEY[1]) {
+                 printf("Certo!\n");
+                 score[i] += 1;
+                 printf("Pontuação: %d\n", score[i]);
+                 printf("\n");
+            } else {
+                 printf("Errado...\n");
+                 printf("Pontuação: %d\n", score[i]);
+                 printf("\n");
+            }
+        }
+
+        printf("A resposta certa era: %s\n", getQuestionList().questionList[1].solution);
+
+        printf("Terceira pergunta:\n");
+        printf("%s\n", getQuestionList().questionList[2].problem);
+        printf("%c. %s\n", KEY[0], getQuestionList().questionList[2].wrongAnswer1);
+        printf("%c. %s\n", KEY[1], getQuestionList().questionList[2].wrongAnswer2);
+        printf("%c. %s\n", KEY[2], getQuestionList().questionList[2].solution);
+        printf("%c. %s\n", KEY[3], getQuestionList().questionList[2].wrongAnswer3);
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            printf("Jogador %d\n", i);
+            scanf(" %c", &answer[i]);
+            printf("\n");
+        }
+
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            if(answer[i] == KEY[2]) {
+                printf("Certo!\n");
+                score[i] += 1;
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            } else {
+                printf("Errado...\n");
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            }
+        }
+
+        printf("A resposta certa era: %s\n", getQuestionList().questionList[2].solution);
+
+        printf("Quarta pergunta:\n");
+        printf("%s\n", getQuestionList().questionList[3].problem);
+        printf("%c. %s\n", KEY[0], getQuestionList().questionList[3].wrongAnswer1);
+        printf("%c. %s\n", KEY[1], getQuestionList().questionList[3].wrongAnswer2);
+        printf("%c. %s\n", KEY[2], getQuestionList().questionList[3].wrongAnswer3);
+        printf("%c. %s\n", KEY[3], getQuestionList().questionList[3].solution);
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            printf("Jogador %d\n", i);
+            scanf(" %c", &answer[i]);
+            printf("\n");
+        }
+
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            if(answer[i] == KEY[3]) {
+                printf("Certo!\n");
+                score[i] += 1;
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            } else {
+                printf("Errado...\n");
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            }
+        }
+
+        printf("A resposta certa era: %s\n", getQuestionList().questionList[3].solution);
+
+        printf("Quinta pergunta:\n");
+        printf("%s\n", getQuestionList().questionList[4].problem);
+        printf("%c. %s\n", KEY[0], getQuestionList().questionList[4].solution);
+        printf("%c. %s\n", KEY[1], getQuestionList().questionList[4].wrongAnswer2);
+        printf("%c. %s\n", KEY[2], getQuestionList().questionList[4].wrongAnswer3);
+        printf("%c. %s\n", KEY[3], getQuestionList().questionList[4].wrongAnswer1);
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            printf("Jogador %d\n", i);
+            scanf(" %c", &answer[i]);
+            printf("\n");
+        }
+
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            if(answer[i] == KEY[0]) {
+                printf("Certo!\n");
+                score[i] += 1;
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            } else {
+                printf("Errado...\n");
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            }
+        }
+
+        printf("A resposta certa era: %s\n", getQuestionList().questionList[4].solution);
+
+        printf("Sexta pergunta:\n");
+        printf("%s\n", getQuestionList().questionList[5].problem);
+        printf("%c. %s\n", KEY[0], getQuestionList().questionList[5].wrongAnswer2);
+        printf("%c. %s\n", KEY[1], getQuestionList().questionList[5].solution);
+        printf("%c. %s\n", KEY[2], getQuestionList().questionList[5].wrongAnswer3);
+        printf("%c. %s\n", KEY[3], getQuestionList().questionList[5].wrongAnswer1);
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            printf("Jogador %d\n", i);
+            scanf(" %c", &answer[i]);
+            printf("\n");
+        }
+
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
             if(answer[i] == KEY[1]) {
                 printf("Certo!\n");
                 score[i] += 1;
@@ -77,6 +203,132 @@ int main() {
                 printf("\n");
             }
         }
+
+        printf("A resposta certa era: %s\n", getQuestionList().questionList[5].solution);
+
+        printf("Sétima pergunta:\n");
+        printf("%s\n", getQuestionList().questionList[6].problem);
+        printf("%c. %s\n", KEY[0], getQuestionList().questionList[6].wrongAnswer2);
+        printf("%c. %s\n", KEY[1], getQuestionList().questionList[6].wrongAnswer3);
+        printf("%c. %s\n", KEY[2], getQuestionList().questionList[6].solution);
+        printf("%c. %s\n", KEY[3], getQuestionList().questionList[6].wrongAnswer1);
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            printf("Jogador %d\n", i);
+            scanf(" %c", &answer[i]);
+            printf("\n");
+        }
+
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            if(answer[i] == KEY[2]) {
+                printf("Certo!\n");
+                score[i] += 1;
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            } else {
+                printf("Errado...\n");
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            }
+        }
+
+        printf("A resposta certa era: %s\n", getQuestionList().questionList[6].solution);
+
+        printf("Oitava pergunta:\n");
+        printf("%s\n", getQuestionList().questionList[7].problem);
+        printf("%c. %s\n", KEY[0], getQuestionList().questionList[7].wrongAnswer2);
+        printf("%c. %s\n", KEY[1], getQuestionList().questionList[7].wrongAnswer3);
+        printf("%c. %s\n", KEY[2], getQuestionList().questionList[7].wrongAnswer1);
+        printf("%c. %s\n", KEY[3], getQuestionList().questionList[7].solution);
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            printf("Jogador %d\n", i);
+            scanf(" %c", &answer[i]);
+            printf("\n");
+        }
+
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            if(answer[i] == KEY[3]) {
+                printf("Certo!\n");
+                score[i] += 1;
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            } else {
+                printf("Errado...\n");
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            }
+        }
+
+        printf("A resposta certa era: %s\n", getQuestionList().questionList[7].solution);
+
+        printf("Nona pergunta:\n");
+        printf("%s\n", getQuestionList().questionList[8].problem);
+        printf("%c. %s\n", KEY[0], getQuestionList().questionList[8].solution);
+        printf("%c. %s\n", KEY[1], getQuestionList().questionList[8].wrongAnswer3);
+        printf("%c. %s\n", KEY[2], getQuestionList().questionList[8].wrongAnswer1);
+        printf("%c. %s\n", KEY[3], getQuestionList().questionList[8].wrongAnswer2);
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            printf("Jogador %d\n", i);
+            scanf(" %c", &answer[i]);
+            printf("\n");
+        }
+
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            if(answer[i] == KEY[0]) {
+                printf("Certo!\n");
+                score[i] += 1;
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            } else {
+                printf("Errado...\n");
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            }
+        }
+
+        printf("A resposta certa era: %s\n", getQuestionList().questionList[8].solution);
+
+        printf("Última pergunta:\n");
+        printf("%s\n", getQuestionList().questionList[9].problem);
+        printf("%c. %s\n", KEY[0], getQuestionList().questionList[9].wrongAnswer3);
+        printf("%c. %s\n", KEY[1], getQuestionList().questionList[9].solution);
+        printf("%c. %s\n", KEY[2], getQuestionList().questionList[9].wrongAnswer1);
+        printf("%c. %s\n", KEY[3], getQuestionList().questionList[9].wrongAnswer2);
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            printf("Jogador %d\n", i);
+            scanf(" %c", &answer[i]);
+            printf("\n");
+        }
+
+        printf("\n");
+
+        for(int i = 1; i < numberOfPlayers + 1; i++) {
+            if(answer[i] == KEY[1]) {
+                printf("Certo!\n");
+                score[i] += 1;
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            } else {
+                printf("Errado...\n");
+                printf("Pontuação: %d\n", score[i]);
+                printf("\n");
+            }
+        }
+
+        printf("A resposta certa era: %s\n", getQuestionList().questionList[9].solution);
 
         printf("Acabou o jogo!\n");
         printf("\n");
--- questions.c
+++ questions.c
@@ -15,22 +15,87 @@ typedef struct questionList {
 struct questionList getQuestionList() {
 
     question q1;
-    strcpy(q1.problem, "Quantos minutos dura um jogo de futebol?");
-    strcpy(q1.solution, "90");
-    strcpy(q1.wrongAnswer1, "70");
-    strcpy(q1.wrongAnswer2, "50");
-    strcpy(q1.wrongAnswer3, "60");
+    strcpy(q1.problem, "Qual foi a primeira selecção a ganhar o campeonato do mundo de futebol da FIFA?");
+    strcpy(q1.solution, "Uruguai");
+    strcpy(q1.wrongAnswer1, "Brasil");
+    strcpy(q1.wrongAnswer2, "França");
+    strcpy(q1.wrongAnswer3, "Inglaterra");
 
     question q2;
     strcpy(q2.problem, "Em que ano foi lançada a Sony Playstation?");
     strcpy(q2.solution, "1994");
-    strcpy(q2.wrongAnswer1, "1989");
+    strcpy(q2.wrongAnswer1, "1992");
     strcpy(q2.wrongAnswer2, "1997");
     strcpy(q2.wrongAnswer3, "1995");
+
+    question q3;
+    strcpy(q3.problem, "Quem foi o primeiro presidente de Portugal após o 25 de Abril?");
+    strcpy(q3.solution, "António de Spínola");
+    strcpy(q3.wrongAnswer1, "Francisco da Costa Gomes");
+    strcpy(q3.wrongAnswer2, "Adelino da Palma Carlos");
+    strcpy(q3.wrongAnswer3, "Mário Soares");
+
+    question q4;
+    strcpy(q4.problem, "Quantas são as leis da termodinâmica?");
+    strcpy(q4.solution, "3");
+    strcpy(q4.wrongAnswer1, "4");
+    strcpy(q4.wrongAnswer2, "2");
+    strcpy(q4.wrongAnswer3, "5");
+
+    question q5;
+    strcpy(q5.problem, "Como se chama o tipo de memória volátil mais utilizado em computadores?");
+    strcpy(q5.solution, "RAM");
+    strcpy(q5.wrongAnswer1, "ROM");
+    strcpy(q5.wrongAnswer2, "Flash");
+    strcpy(q5.wrongAnswer3, "SSD");
+
+    question q6;
+    strcpy(q6.problem, "Qual é a capital do Cazaquistão?");
+    strcpy(q6.solution, "Astana");
+    strcpy(q6.wrongAnswer1, "Nur-Sultan");
+    strcpy(q6.wrongAnswer2, "Almaty");
+    strcpy(q6.wrongAnswer3, "Aktobe");
+
+    question q7;
+    strcpy(q7.problem, "Qual é o termo correcto para designar uma descida gradual da taxa de inflação?");
+    strcpy(q7.solution, "Desinflação");
+    strcpy(q7.wrongAnswer1, "Deflação");
+    strcpy(q7.wrongAnswer2, "Estagflção");
+    strcpy(q7.wrongAnswer3, "Desvalorização");
+
+    question q8;
+    strcpy(q8.problem, "O que significa em português a palavra latina \"parvum\"?");
+    strcpy(q8.solution, "Pequeno");
+    strcpy(q8.wrongAnswer1, "Parvo");
+    strcpy(q8.wrongAnswer2, "Estúpido");
+    strcpy(q8.wrongAnswer3, "Burro");
+
+    question q9;
+    strcpy(q9.problem, "Qual era o nome civil do papa católico João XXI?");
+    strcpy(q9.solution, "Pedro Julião");
+    strcpy(q9.wrongAnswer1, "Nicholas Breakspear");
+    strcpy(q9.wrongAnswer2, "Albino Luciani");
+    strcpy(q9.wrongAnswer3, "Pierre de Beaufort");
+
+    question q10;
+    strcpy(q10.problem, "Qual foi a produção que tornou conhecida mundialmente a actriz norte-americana "
+                        "Jennifer Coolidge?");
+    strcpy(q10.solution, "American Pie");
+    strcpy(q10.wrongAnswer1, "The White Lotus");
+    strcpy(q10.wrongAnswer2, "2 Broke Girls");
+    strcpy(q10.wrongAnswer3, "Joey");
 
     questionList ql;
     ql.questionList[0] = q1;
     ql.questionList[1] = q2;
+    ql.questionList[2] = q3;
+    ql.questionList[3] = q4;
+    ql.questionList[4] = q5;
+    ql.questionList[5] = q6;
+    ql.questionList[6] = q7;
+    ql.questionList[7] = q8;
+    ql.questionList[8] = q9;
+    ql.questionList[9] = q10;
 
     return ql;
 };
